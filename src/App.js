9c877f752c97d551c1c694c7deb947f664e002a1
wenyangzhang42 <wenyang.zhang42@gmail.com>
2022-11-05 17:22:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
+import { createTheme, ThemeProvider} from "@mui/material";
 import './App.css';
 
+const theme = createTheme({
+  palette:{
+    primary:{
+      main: "#2e1667",
+    },
+    secondary:{
+      main:"#c7d8ed"
+    },
+  },
+  typography:{
+    fontFamily:[
+        'Roboto'
+    ],
+    h4:{
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+    },
+    h5:{
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <div className="App">
+        <ThemeProvider theme={theme}>
+
+        </ThemeProvider>
+      </div>
+  )
 }
 
 export default App;
